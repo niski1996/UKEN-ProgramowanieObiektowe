149e6ff5481fab12da0994291de32200562750bc
niski1996 <niski1996@interia.pl>
2025-06-07 20:02:35 +0200

    Autogenerated commit

--- lab-07-wskazniki/funkcje.cpp
+++ lab-07-wskazniki/funkcje.cpp
@@ -18,7 +18,7 @@ double los_double()
 {
 	static std::default_random_engine silnik(std::chrono::system_clock::now().time_since_epoch().count());
 
-	std::normal_distribution<double> gauss (170, 10);
+	std::normal_distribution<double> gauss(170, 10);
 	return gauss(silnik);
 }
 
@@ -26,7 +26,7 @@ unsigned int los_uint()
 {
 	static std::default_random_engine silnik(std::chrono::system_clock::now().time_since_epoch().count());
 
-	std::uniform_int_distribution<unsigned int> jednorodny (10, 20);
+	std::uniform_int_distribution<unsigned int> jednorodny(10, 20);
 	return jednorodny(silnik);
 }
 
@@ -38,12 +38,12 @@ void separator()
 }
 
 //////////////////////////////////////////
-std::shared_ptr<double []> alokuj_tablice (const unsigned int n)
+std::shared_ptr<double[]> alokuj_tablice(const unsigned int n)
 {
-	return std::shared_ptr<double[]> (new double[n]);
+	return std::shared_ptr<double[]>(new double[n]);
 }
 
-double * alokuj_tablice_zwykly_wskaznik (const unsigned int n)
+double *alokuj_tablice_zwykly_wskaznik(const unsigned int n)
 {
 	return new double[n];
 }
@@ -54,33 +54,120 @@ void wypelnij(std::shared_ptr<double[]> p, const int n)
 		p[i] = los_double();
 }
 
-void wypelnij(double * p, const int n)
+void wypelnij(double *p, const int n)
 {
 	for (int i = 0; i < n; i++)
 		p[i] = los_double();
 }
 
-void wypisz(const double * p, const int n)
+void wypisz(const double *p, const int n)
 {
+	for (int i = 0; i < n; ++i)
+	{
+		std::cout << p[i] << std::endl;
+	}
+	std::cout << std::endl;
 }
 
 void wypisz(std::shared_ptr<double[]> p, const int n)
 {
+	for (int i = 0; i < n; ++i)
+	{
+		std::cout << p[i] << " ";
+	}
 }
 
-std::pair<double, double> usun_min_max (std::shared_ptr<double[]> * p, unsigned int * n)
+std::pair<double, double> usun_min_max(std::shared_ptr<double[]> *p, unsigned int *n)
 {
-	return {0,0};
+	if (*n == 0)
+		return {0, 0};
+
+	unsigned int size = *n;
+	double *arr = p->get();
+
+	double min_val = arr[0];
+	double max_val = arr[0];
+	unsigned int min_idx = 0;
+	unsigned int max_idx = 0;
+
+	for (unsigned int i = 1; i < size; ++i)
+	{
+		if (arr[i] < min_val)
+		{
+			min_val = arr[i];
+			min_idx = i;
+		}
+		if (arr[i] > max_val)
+		{
+			max_val = arr[i];
+			max_idx = i;
+		}
+	}
+
+	unsigned int new_size = size - (min_idx == max_idx ? 1 : 2);
+	if (new_size == 0)
+	{
+		*p = nullptr;
+		*n = 0;
+		return {min_val, max_val};
+	}
+
+	std::shared_ptr<double[]> new_array(new double[new_size]);
+	unsigned int j = 0;
+	for (unsigned int i = 0; i < size; ++i)
+	{
+		if (i != min_idx && i != max_idx)
+		{
+			new_array[j++] = arr[i];
+		}
+	}
+
+	*p = new_array;
+	*n = new_size;
+	return {min_val, max_val};
 }
 
-void push_back(std::shared_ptr<double[]> * p, unsigned int * n, const double d)
+void push_back(std::shared_ptr<double[]> *p, unsigned int *n, const double d)
 {
+	unsigned int new_size = *n + 1;
+	std::shared_ptr<double[]> new_array(new double[new_size]);
+	for (unsigned int i = 0; i < *n; ++i)
+	{
+		new_array[i] = (*p)[i];
+	}
+	new_array[*n] = d;
+	*p = new_array;
+	*n = new_size;
 }
 
-void pop_back(std::shared_ptr<double[]> * p, unsigned int * n)
+void pop_back(std::shared_ptr<double[]> *p, unsigned int *n)
 {
+	if (*n == 0)
+		return;
+	unsigned int new_size = *n - 1;
+	if (new_size == 0)
+	{
+		*p = nullptr;
+		*n = 0;
+		return;
+	}
+	std::shared_ptr<double[]> new_array(new double[new_size]);
+	for (unsigned int i = 0; i < new_size; ++i)
+	{
+		new_array[i] = (*p)[i];
+	}
+	*p = new_array;
+	*n = new_size;
 }
 
-void wypisz(const std::vector<std::pair<std::shared_ptr<double[]> , unsigned int>> & t)
+void wypisz(const std::vector<std::pair<std::shared_ptr<double[]>, unsigned int>> &t)
 {
+	for (const auto &[ptr, size] : t)
+	{
+		for (unsigned int i = 0; i < size; ++i)
+		{
+			std::cout << ptr[i] << " ";
+		}
+		std::cout << std::endl;
+	}
 }
--- lab-07-wskazniki/main.cpp
+++ lab-07-wskazniki/main.cpp
@@ -14,78 +14,78 @@
 ///////////////////////////////////////////
 int main ()
 {
-	// unsigned int rozmiar = los_uint();
-	// double * pTabZwWsk = alokuj_tablice_zwykly_wskaznik(rozmiar); // Funkcja alokuje tablice o zadanym rozmiarze.
-	// wypelnij (pTabZwWsk, rozmiar);  // Funkcja wypelnia tablice losowymi wartosciami.
-	// wypisz (pTabZwWsk, rozmiar); // Funkcja wypisuje tablice na standardowe wyjscie.
-	// delete [] pTabZwWsk; // MUSIMY zwolnic pamiec!
+	unsigned int rozmiar = los_uint();
+	double * pTabZwWsk = alokuj_tablice_zwykly_wskaznik(rozmiar); // Funkcja alokuje tablice o zadanym rozmiarze.
+	wypelnij (pTabZwWsk, rozmiar);  // Funkcja wypelnia tablice losowymi wartosciami.
+	wypisz (pTabZwWsk, rozmiar); // Funkcja wypisuje tablice na standardowe wyjscie.
+	delete [] pTabZwWsk; // MUSIMY zwolnic pamiec!
 
 	separator();
 
-	// std::shared_ptr<double []> pTab = alokuj_tablice(rozmiar); // Funkcja alokuje tablice o zadanym rozmiarze.
-	// wypelnij (pTab, rozmiar);  // Funkcja wypelnia tablice losowymi wartosciami.
-	// wypisz (pTab, rozmiar);    // Funkcja wypisuje tablice na standardowe wyjscie.
+	std::shared_ptr<double []> pTab = alokuj_tablice(rozmiar); // Funkcja alokuje tablice o zadanym rozmiarze.
+	wypelnij (pTab, rozmiar);  // Funkcja wypelnia tablice losowymi wartosciami.
+	wypisz (pTab, rozmiar);    // Funkcja wypisuje tablice na standardowe wyjscie.
 
 	separator();
 
-	// for (int i = 0; i < 6; i++)
-	// {
-	// 	double nowa = los_double();
-	// 	std::cout << nowa << std::endl;
-	// 	push_back(&pTab, &rozmiar, nowa);  // Funkcja dodaje nowa wartosc na koniec tablicy. Zwieksza tym samym rozmiar tablicy.
-	// 	wypisz(pTab, rozmiar);
-	// }
+	for (int i = 0; i < 6; i++)
+	{
+		double nowa = los_double();
+		std::cout << nowa << std::endl;
+		push_back(&pTab, &rozmiar, nowa);  // Funkcja dodaje nowa wartosc na koniec tablicy. Zwieksza tym samym rozmiar tablicy.
+		wypisz(pTab, rozmiar);
+	}
 
 	separator();
 
-	// while (rozmiar)
-	// {
-	// 	pop_back(&pTab, &rozmiar); // Funkcja usuwa ostatnia wartosc z tablicy. Zmniejsza tym samym jej rozmiar.
-	// 	wypisz(pTab, rozmiar);
-	// }
-
-	// separator();
-
-	// pTab = alokuj_tablice(rozmiar);
-	// wypisz(pTab, rozmiar);
-
-	// for (int i = 0; i < 10; i++)
-	// {
-	// 	double nowa = los_double();
-	// 	std::cout << nowa << std::endl;
-	// 	push_back(&pTab, &rozmiar, nowa);
-	// 	wypisz(pTab, rozmiar);
-	// }
+	while (rozmiar)
+	{
+		pop_back(&pTab, &rozmiar); // Funkcja usuwa ostatnia wartosc z tablicy. Zmniejsza tym samym jej rozmiar.
+		wypisz(pTab, rozmiar);
+	}
 
 	separator();
 
-	// while (rozmiar > 0)
-	// {
-	// 	wypisz(pTab, rozmiar);
-	// 	std::cout << "rozmiar: " << rozmiar << std::endl;
-	// 	auto [mini, maxi] = usun_min_max(&pTab, &rozmiar); // Funkcja usuwa z tablicy wartosc minimalną i maksymalną. Skraca tym samym tablice. Jezeli maksimum i minimum sa takie same, funkcja usuwa tylko jedna wartosc.
-	// 	std::cout << "mini: " << mini << std::endl;
-	// 	std::cout << "maxi: " << maxi << std::endl;
-	// }
+	pTab = alokuj_tablice(rozmiar);
+	wypisz(pTab, rozmiar);
+
+	for (int i = 0; i < 10; i++)
+	{
+		double nowa = los_double();
+		std::cout << nowa << std::endl;
+		push_back(&pTab, &rozmiar, nowa);
+		wypisz(pTab, rozmiar);
+	}
 
 	separator();
 
-  // rozmiar = los_uint();
+	while (rozmiar > 0)
+	{
+		wypisz(pTab, rozmiar);
+		std::cout << "rozmiar: " << rozmiar << std::endl;
+		auto [mini, maxi] = usun_min_max(&pTab, &rozmiar); // Funkcja usuwa z tablicy wartosc minimalną i maksymalną. Skraca tym samym tablice. Jezeli maksimum i minimum sa takie same, funkcja usuwa tylko jedna wartosc.
+		std::cout << "mini: " << mini << std::endl;
+		std::cout << "maxi: " << maxi << std::endl;
+	}
 
-	// typedef std::shared_ptr<double[]> pTabD;
-	// typedef std::pair<pTabD, unsigned int> tablica; // typ: tablica z rozmiarem
+	separator();
 
-	// std::vector<tablica> tablice_w_wektorze (rozmiar); // std::vector<std::pair<std::shared_ptr<double[]>, unsigned int>> tablice_w_wektorze (rozmiar);
-	// for (auto it = tablice_w_wektorze.begin(); it != tablice_w_wektorze.end(); it++)
-	// {
-	// 	unsigned int n = los_uint();
-	// 	it->first = alokuj_tablice(n);
-	// 	it->second = n;
-	// 	wypelnij(it->first, it->second);
-	// }
+  rozmiar = los_uint();
 
-	// std::cout << rozmiar << " tablic w wektorze" << std::endl;
-	// wypisz(tablice_w_wektorze);
+	typedef std::shared_ptr<double[]> pTabD;
+	typedef std::pair<pTabD, unsigned int> tablica; // typ: tablica z rozmiarem
+
+	std::vector<tablica> tablice_w_wektorze (rozmiar); // std::vector<std::pair<std::shared_ptr<double[]>, unsigned int>> tablice_w_wektorze (rozmiar);
+	for (auto it = tablice_w_wektorze.begin(); it != tablice_w_wektorze.end(); it++)
+	{
+		unsigned int n = los_uint();
+		it->first = alokuj_tablice(n);
+		it->second = n;
+		wypelnij(it->first, it->second);
+	}
+
+	std::cout << rozmiar << " tablic w wektorze" << std::endl;
+	wypisz(tablice_w_wektorze);
 
 	return 0;
 }
